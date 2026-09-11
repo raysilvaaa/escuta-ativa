a8291357e4b91ba29ad2f1f731ffa8b4110427ca
raysilvaaa <rayaanesiilva2017@gmail.com>
2026-09-11 16:11:43 -0300

    Update SiteNav.js

--- components/SiteNav.js
+++ components/SiteNav.js
@@ -14,10 +14,10 @@ export default function SiteNav() {
     <nav className="site-nav">
       <div className="site-nav-inner">
         <Link href="/">Início</Link>
-        <Link href="/sobre">Sobre o atendimento</Link>
         <Link href="/agendamento">Agendamento</Link>
+        <Link href="/sobre">Sobre o atendimento</Link>
+        <Link href="/faq">Dúvidas</Link>
         <Link href="/contato">Contato</Link>
-        <Link href="/faq">Perguntas frequentes</Link>
       </div>
     </nav>
   );
